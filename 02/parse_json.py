c1dab1770a65343cdab104459edd9730dbd15aa6
EternalTLD <shvejkin01@mail.ru>
2023-10-12 13:10:30 +0300

    02 №2

--- 02/parse_json.py
+++ 02/parse_json.py
@@ -2,11 +2,20 @@ import json
 
 
 def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=None):
+    if not isinstance(json_str, str):
+        raise TypeError("Variable 'json_str' must be str")
+    if keyword_callback is None:
+        raise TypeError("Variable 'keyword_callback' should not be None")
+    if required_fields is None:
+        raise TypeError("Variable 'required_fields' should not be None")
+    if keywords is None:
+        raise TypeError("Variable 'keywords' should not be None")
+
     json_doc = json.loads(json_str)
     for field in required_fields:
         if field in json_doc.keys():
             value_str = json_doc[field]
             value_list = value_str.split()
-            for value in value_list:
-                if value.lower() in map(lambda keyword: keyword.lower(), keywords):
-                    keyword_callback(value)
+            for keyword in keywords:
+                if keyword.lower() in map(lambda value: value.lower(), value_list):
+                    keyword_callback(field, keyword)
--- 02/test_parse_json.py
+++ 02/test_parse_json.py
@@ -1,140 +1,162 @@
-import json
-import os
+from unittest.mock import call
+import pytest
 
 from parse_json import parse_json
 
 
-class TestCallback:
-    def callback(self, keyword):
-        return "Called for " + keyword
-
-
-def setup_json_file(data):
-    with open("test_json.json", "w", encoding="utf-8") as file_object:
-        file_object.write(json.dumps(data))
-
-
-def test_callback_return(mocker):
-    json_data = {"key1": "word1 word2", "key2": "word2 word3"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
-    required_fields = ["key1"]
-    keywords = ["word2"]
-    parse_json(json_str, callback, required_fields, keywords)
-    assert callback.spy_return == "Called for word2"
-    os.remove("test_json.json")
-
-
 def test_one_keyword(mocker):
-    json_data = {"key1": "word2 word1", "key2": "word1 word3"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "word2 word1", "key2": "word1 word3"}'
+    callback = mocker.Mock()
     required_fields = ["key1", "key2"]
     keywords = ["word1"]
     parse_json(json_str, callback, required_fields, keywords)
+    expected_calls = [call("key1", "word1"), call("key2", "word1")]
+    assert callback.call_args_list == expected_calls
     assert callback.call_count == 2
-    os.remove("test_json.json")
 
 
 def test_many_keywords(mocker):
-    json_data = {"key1": "word2 word1", "key2": "word1 word3"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "word2 word1", "key2": "word1 word3"}'
+    callback = mocker.Mock()
     required_fields = ["key1", "key2"]
     keywords = ["word1", "word3", "word4"]
     parse_json(json_str, callback, required_fields, keywords)
+    expected_calls = [
+        call("key1", "word1"),
+        call("key2", "word1"),
+        call("key2", "word3"),
+    ]
+    assert callback.call_args_list == expected_calls
     assert callback.call_count == 3
-    os.remove("test_json.json")
+
+
+def test_many_same_keywords_in_one_field(mocker):
+    json_str = '{"key1": "word2 word1 word2", "key2": "word3 word3 word3"}'
+    callback = mocker.Mock()
+    required_fields = ["key1", "key2"]
+    keywords = ["word2", "word3"]
+    parse_json(json_str, callback, required_fields, keywords)
+    expected_calls = [call("key1", "word2"), call("key2", "word3")]
+    assert callback.call_args_list == expected_calls
+    assert callback.call_count == 2
+
+
+def test_no_keywords_in_fields(mocker):
+    json_str = '{"key1": "word2 word1 word2", "key2": "word3 word3 word3"}'
+    callback = mocker.Mock()
+    required_fields = ["key1", "key2"]
+    keywords = ["word4", "word5"]
+    parse_json(json_str, callback, required_fields, keywords)
+    assert not callback.called
 
 
 def test_empty_keywords(mocker):
-    json_data = {"key1": "word2 word1", "key2": "word1 word3"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "word2 word1", "key2": "word1 word3"}'
+    callback = mocker.Mock()
     required_fields = ["Key1", "key2"]
     keywords = []
     parse_json(json_str, callback, required_fields, keywords)
-    assert callback.call_count == 0
-    os.remove("test_json.json")
+    assert not callback.called
 
 
 def test_keywords_case_insensitive(mocker):
-    json_data = {"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}'
+    callback = mocker.Mock()
     required_fields = ["key1", "key2"]
     keywords = ["wOrD1", "word2", "WORD3"]
     parse_json(json_str, callback, required_fields, keywords)
-    assert callback.call_count == 5
-    os.remove("test_json.json")
+    expected_calls = [
+        call("key1", "wOrD1"),
+        call("key1", "word2"),
+        call("key2", "wOrD1"),
+        call("key2", "WORD3"),
+    ]
+    assert callback.call_args_list == expected_calls
+    assert callback.call_count == 4
 
 
 def test_keywords_match(mocker):
-    json_data = {"key1": "tester testing", "key2": "beta-test test"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
+    json_str = '{"key1": "tester testing", "key2": "beta-test test"}'
     required_fields = ["key1", "key2"]
-    callback = mocker.spy(TestCallback(), "callback")
+    callback = mocker.Mock()
 
     keywords = ["testinggg"]
     parse_json(json_str, callback, required_fields, keywords)
-    assert callback.call_count == 0
+    assert not callback.called
 
     keywords = ["tes"]
     parse_json(json_str, callback, required_fields, keywords)
-    assert callback.call_count == 0
+    assert not callback.called
 
     keywords = ["test"]
     parse_json(json_str, callback, required_fields, keywords)
+    assert callback.call_args_list == [call("key2", "test")]
     assert callback.call_count == 1
-    os.remove("test_json.json")
 
 
 def test_one_required_field(mocker):
-    json_data = {"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}'
+    callback = mocker.Mock()
     required_fields = ["key1"]
     keywords = ["wOrD1", "word2", "WORD3"]
     parse_json(json_str, callback, required_fields, keywords)
     assert callback.call_count == 2
-    os.remove("test_json.json")
+    assert callback.call_args_list == [call("key1", "wOrD1"), call("key1", "word2")]
 
 
 def test_empty_required_fields(mocker):
-    json_data = {"key1": "word2 word1", "key2": "word1 word3"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "word2 word1", "key2": "word1 word3"}'
+    callback = mocker.Mock()
     required_fields = []
     keywords = ["wOrD1", "WORD3"]
     parse_json(json_str, callback, required_fields, keywords)
-    assert callback.call_count == 0
-    os.remove("test_json.json")
+    assert not callback.called
 
 
 def test_required_fields_case_sensitive(mocker):
-    json_data = {"key1": "word2 word1 word3", "key2": "word1 word3"}
-    setup_json_file(json_data)
-    with open("test_json.json", "r", encoding="utf-8") as file_object:
-        json_str = file_object.read()
-    callback = mocker.spy(TestCallback(), "callback")
+    json_str = '{"key1": "word2 word1 word3", "key2": "word1 word3"}'
+    callback = mocker.Mock()
     required_fields = ["KEY1"]
     keywords = ["WORD3"]
     parse_json(json_str, callback, required_fields, keywords)
-    assert callback.call_count == 0
-    os.remove("test_json.json")
+    assert not callback.called
+
+
+@pytest.mark.parametrize("json_str", [1, 0.1, (1, 2, 3), [1, 2, 3], {"1": 1}, None])
+def test_json_str_type(mocker, json_str):
+    callback = mocker.Mock()
+    required_fields = ["key1"]
+    keywords = ["word1"]
+    with pytest.raises(TypeError) as exception:
+        assert parse_json(json_str, callback, required_fields, keywords)
+    assert "Variable 'json_str' must be str" in str(exception.value)
+
+
+def test_keyword_callback_is_none():
+    json_str = '{"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}'
+    callback = None
+    required_fields = ["key1"]
+    keywords = ["word1"]
+    with pytest.raises(TypeError) as exception:
+        assert parse_json(json_str, callback, required_fields, keywords)
+    assert "Variable 'keyword_callback' should not be None" in str(exception.value)
+
+
+def test_required_fields_is_none(mocker):
+    json_str = '{"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}'
+    callback = mocker.Mock()
+    required_fields = None
+    keywords = ["word1"]
+    with pytest.raises(TypeError) as exception:
+        assert parse_json(json_str, callback, required_fields, keywords)
+    assert "Variable 'required_fields' should not be None" in str(exception.value)
+
+
+def test_keywords_is_none(mocker):
+    json_str = '{"key1": "Word2 word1", "key2": "wORd1 word3 word1", "key3": "woRD2"}'
+    callback = mocker.Mock()
+    required_fields = ["key1"]
+    keywords = None
+    with pytest.raises(TypeError) as exception:
+        assert parse_json(json_str, callback, required_fields, keywords)
+    assert "Variable 'keywords' should not be None" in str(exception.value)
