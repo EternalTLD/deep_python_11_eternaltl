e410007560fbcaa84a2da97b59183d2917f23d93
EternalTLD <shvejkin01@mail.ru>
2023-10-16 13:27:58 +0300

    05

--- 05/lru.py
+++ 05/lru.py
@@ -1,19 +1,33 @@
 class LRUCache:
     def __init__(self, limit=42):
         self.limit = limit
-        self.cache = dict()
-        
+        self.cache_dict = {}
+
+    @property
+    def limit(self):
+        return self._limit
+
+    @limit.setter
+    def limit(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Cache limit must be int")
+        if value <= 0:
+            raise ValueError("Cache limit must be greater then 0")
+        self._limit = value
+
     def get(self, key):
-        if not self.cache.get(key):
-            raise ValueError(f"There is no {key}")
-        self.cache[key] = self.cache.pop(key)
-        return self.cache[key]
+        if self.cache_dict.get(key):
+            self.cache_dict[key] = self.cache_dict.pop(key)
+            return self.cache_dict[key]
+        return None
 
     def set(self, key, value):
-        if len(self.cache) == self.limit:
-            lru_key = list(self.cache.keys())[0]
-            self.cache.pop(lru_key)
-            self.cache[key] = value
+        if key in self.cache_dict:
+            self.cache_dict.pop(key)
+            self.cache_dict[key] = value
             return True
-        self.cache[key] = value
+        if len(self.cache_dict) == self.limit and key not in self.cache_dict:
+            key_to_delete = next(iter(self.cache_dict))
+            self.cache_dict.pop(key_to_delete)
+        self.cache_dict[key] = value
         return True
---
+++ 05/test_lru.py
@@ -0,0 +1,75 @@
+import pytest
+
+from lru import LRUCache
+
+
+def test_cache_keys_order():
+    cache = LRUCache(3)
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+    cache.set("k3", "val3")
+    assert list(cache.cache_dict.keys()) == ["k1", "k2", "k3"]
+
+    cache.set("k2", "val2")
+    assert list(cache.cache_dict.keys()) == ["k1", "k3", "k2"]
+
+    cache.set("k1", "val1")
+    assert list(cache.cache_dict.keys()) == ["k3", "k2", "k1"]
+
+    cache.set("k3", "val1")
+    assert list(cache.cache_dict.keys()) == ["k2", "k1", "k3"]
+
+
+def test_cache_get_and_set():
+    cache = LRUCache(2)
+
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+
+    assert cache.get("k3") is None
+    assert cache.get("k2") == "val2"
+    assert cache.get("k1") == "val1"
+
+    cache.set("k3", "val3")
+
+    assert cache.get("k3") == "val3"
+    assert cache.get("k2") is None
+    assert cache.get("k1") == "val1"
+
+
+def test_update_existing_key():
+    cache = LRUCache(2)
+    cache.set("k1", "val1")
+    assert cache.get("k1") == "val1"
+
+    cache.set("k1", "new_val1")
+    assert cache.get("k1") == "new_val1"
+
+
+def test_cache_limit_value():
+    with pytest.raises(ValueError) as exception:
+        assert LRUCache(0)
+    assert "Cache limit must be greater then 0" in str(exception.value)
+
+    with pytest.raises(ValueError) as exception:
+        assert LRUCache(-999)
+    assert "Cache limit must be greater then 0" in str(exception.value)
+
+    cache = LRUCache(2)
+    with pytest.raises(ValueError) as exception:
+        cache.limit = -1
+    assert "Cache limit must be greater then 0" in str(exception.value)
+    assert cache.limit == 2
+
+
+@pytest.mark.parametrize("limit", ["1", 0.1, [1, 2, 3], (1, 2, 3), {"1": 1}, None])
+def test_cache_limit_type(limit):
+    with pytest.raises(TypeError) as exception:
+        assert LRUCache(limit)
+    assert "Cache limit must be int" in str(exception.value)
+
+    cache = LRUCache(2)
+    with pytest.raises(TypeError) as exception:
+        cache.limit = limit
+    assert "Cache limit must be int" in str(exception.value)
+    assert cache.limit == 2
