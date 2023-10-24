08ae0d73e6df1bcef0e5a7854a5a0922deabcf72
EternalTLD <shvejkin01@mail.ru>
2023-10-24 23:19:34 +0300

    06

--- 06/client.py
+++ 06/client.py
@@ -1,24 +1,19 @@
 import socket
 import threading
+from queue import Queue
 import time
+
 
 class Client:
 
-    def __init__(self, threads_number=3, file='06/test.txt', host='127.0.0.1', port=2000) -> None:
+    def __init__(self, threads_number=5, file='06/test.txt', host='127.0.0.1', port=2000) -> None:
         self.threads_number = threads_number
         self.file = file
         self.host = host
         self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.task_queue = Queue()
 
-    def send(self):
-        self.socket.connect((self.host, self.port))
-        print(f"[CONNECTED] - Connected by {(self.host, self.port)}")
-        self.run_threads()
-        data = self.socket.recv(1024)
-        print(data.decode('utf-8'))
-
-    def run_threads(self):
+    def work(self):
         threads = [
             threading.Thread(target=self.send_urls)
             for _ in range(self.threads_number)
@@ -27,15 +22,34 @@ class Client:
         for thread in threads:
             thread.start()
 
+        self.fetch_urls()
+
         for thread in threads:
             thread.join()
 
     def send_urls(self):
+        while True:
+            client_socket = socket.socket()
+            client_socket.connect((self.host, self.port))
+            thread = threading.current_thread()
+            url = self.task_queue.get()
+            # if url is None:
+            #     self.queue.put(None)
+            #     print(f"Thread - {thread.name} is stopped")
+            #     return
+            print(f"{url} from thread - {thread.name}")
+            url = url.encode() + '\n'.encode()
+            client_socket.send(url)
+            data = client_socket.recv(4096).decode('utf-8')
+            print(data)
+
+    def fetch_urls(self):
         with open(file=self.file, encoding='utf-8') as file_object:
             for url in file_object:
-                self.socket.sendall(url.encode())
+                self.task_queue.put(url.strip())
+            self.task_queue.put(None)
 
 
 if __name__ == "__main__":
     client = Client()
-    client.send()
+    client.work()
--- 06/server.py
+++ 06/server.py
@@ -1,39 +1,51 @@
 import socket
 import threading
+from queue import Queue
+import time
 
 
 class Server:
 
-    def __init__(self, workers=1, host='127.0.0.1', port=2000) -> None:
+    def __init__(self, workers_number=1, host='127.0.0.1', port=2000) -> None:
         self.host = host
         self.port = port
-        self.workers = workers
+        self.workers_number = workers_number
+        self.queue = Queue()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.host, self.port))
+        self.res_queue = Queue()
 
     def start(self):
         self.socket.listen(5)
-        while True:
-            client_socket, address = self.socket.accept()
-            thread = threading.Thread(target=self._listen_client, args=(client_socket, address))
-            thread.start()
-            print(f"[NEW CONNECTION {address}] - Thread {thread.native_id}")
+        client_socket, address = self.socket.accept()
+        for i in range(self.workers_number):
+            worker = threading.Thread(target=self.proccess_url, name=f'Worker {i}')
+            worker.start()
+        resper = threading.Thread(target=self.send_resp, name="resp")
+        resper.start()
+        listener = threading.Thread(target=self.listen_client, args=(client_socket, address), name="Listener")
+        listener.start()
 
-    def _listen_client(self, client_socket, addres):
+    def listen_client(self, client_socket, addres):
         while True:
-            try:
-                data = client_socket.recv(4096)
-                if data:
-                    client_socket.send('[SERVER] - Data from server: '.encode() + data)
-                    print(data.decode('utf-8'))
-                else:
-                    raise Exception('Client disconnected')
-            except:
-                client_socket.close()
-                return False
-                
+            client_data = client_socket.recv(4096)
+            if client_data:
+                sep_data = client_data.decode().split('\n')
+                for i in range(len(sep_data)):
+                    if sep_data[i] != '':
+                        self.queue.put(sep_data[i])
 
+    def proccess_url(self):
+        while True:
+            th = threading.current_thread()
+            url = self.queue.get()
+            resp = f"SERVER proccessed {url} TH - {th.name}"
+            self.res_queue.put(resp)
+
+    def send_resp(self):
+        while True:
+            resp = self.res_queue.get()
+            print(resp)
 
 if __name__ == "__main__":
     server = Server()
